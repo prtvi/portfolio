1841f0e099dabeb85ce03885078598481b34111e
prtvi <prithvippv25@gmail.com>
2022-07-24 01:56:11 +0530

    add testimonial-modal, clients, Resume, init Portfolio

--- v2/script.js
+++ v2/script.js
@@ -11,3 +11,61 @@ const sidebarBtn = document.querySelector('[data-sidebar-btn]');
 // sidebar toggle functionality for mobile
 
 sidebarBtn.addEventListener('click', () => elementToggleFunc(sidebar));
+
+// 
+// 
+// 
+// 
+// all for modal action
+
+// testimonials variables
+
+const testimonialsItems = document.querySelectorAll('[data-testimonials-item]');
+const modalContainer = document.querySelector('[data-modal-container]');
+const modalCloseBtn = document.querySelector('[data-modal-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
+
+// modal variables
+
+const modalImg = document.querySelector('[data-modal-img]');
+const modalTitle = document.querySelector('[data-modal-title]');
+const modalText = document.querySelector('[data-modal-text]');
+
+// modal toggle function
+
+const testimonialsModalFunc = function () {
+  overlay.classList.toggle('active');
+  modalContainer.classList.toggle('active');
+};
+
+// add click events to all modal items
+
+testimonialsItems.forEach(function (t) {
+  t.addEventListener('click', function () {
+    modalImg.src = this.querySelector('[data-testimonials-avatar]').src;
+    modalImg.alt = this.querySelector('[data-testimonials-avatar]').alt;
+
+    modalTitle.innerHTML = this.querySelector(
+      '[data-testimonials-title]'
+    ).innerHTML;
+    modalText.innerHTML = this.querySelector(
+      '[data-testimonials-text]'
+    ).innerHTML;
+
+    testimonialsModalFunc();
+  });
+});
+
+// add click event to close modal
+
+modalCloseBtn.addEventListener('click', testimonialsModalFunc);
+overlay.addEventListener('click', testimonialsModalFunc);
+
+
+// 
+// 
+// 
+// 
+// 
+// 
+// 
